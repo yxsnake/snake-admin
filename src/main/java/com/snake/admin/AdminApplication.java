34b7beac175cd23ce3b0fba7372687e639b259b1
snake <370696614@qq.com>
2024-07-31 21:42:19 +0800

    修改配置

--- src/main/java/com/snake/admin/AdminApplication.java
+++ src/main/java/com/snake/admin/AdminApplication.java
@@ -1,2 +1,58 @@
-package com.snake.admin;public class AdminApplication {
+package com.snake.admin;
+
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author: snake
+ * @create-time: 2024-07-16
+ * @description: 启动类
+ * @version: 1.0
+ */
+@Slf4j
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(AdminApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StrUtil.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("无法确定主机地址，使用默认值'localhost'.");
+        }
+        String docSuffix = "doc.html";
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}{}\n\t" +
+                        "External: \thttp://{}:{}{}\n\t" +
+                        "ApiDoc: \t\thttp://localhost:{}{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                serverPort,
+                contextPath,
+                hostAddress,
+                serverPort,
+                contextPath,
+                serverPort,
+                contextPath,
+                docSuffix,
+                env.getActiveProfiles());
+    }
 }
